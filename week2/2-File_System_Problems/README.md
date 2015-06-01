b6f5d157dd364e6a5ead7052914624e276c33ab8
radosz <radosz88@gmail.com>
2015-06-01 19:26:09 +0300

    README and duhs is  fixed

--- week2/2-File_System_Problems/README.md
+++ week2/2-File_System_Problems/README.md
@@ -27,6 +27,35 @@ Python is an awesome language!
 You should try it.
 This is the result:
 
+Problems with files
+Implement the cat command - Print file contents
+
+In Linux, there is a very useful command, called cat:
+
+$ cat file.txt
+This is some file
+And cat is printing it's contents
+Implement a Python script, called cat.py that takes one argument - a filename and prints the contents of that file to the console.
+
+Boilerplate
+
+# cat.py
+import sys
+
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
+Examples
+
+If we have file.txt in the same directory with cat.py, and file.txt is with the following text:
+
+Python is an awesome language!
+You should try it.
+This is the result:
+
 $ python3.4 cat.py file.txt
 Python is an awesome language!
 You should try it.
--- week2/2-File_System_Problems/duhs.py
+++ week2/2-File_System_Problems/duhs.py
@@ -3,13 +3,13 @@ import os
 import math
 
 
-def convert_size(size):
+def convert_size(size,work_dir):
     size_name = ("KB", "MB", "GB", "TB")
     i = int(math.floor(math.log(size, 1024)))
     p = math.pow(1024, i)
     s = round(size / p, 2)
     if (s > 0):
-        return "{} {}".format(s, size_name[i])
+        return "{} size is: {} {}".format(work_dir,s,size_name[i])
     else:
         return '0B'
 
@@ -21,9 +21,9 @@ def main():
         os.chdir(path)
         files_in_dir = os.listdir(os.getcwd())
         size_list = [os.path.getsize(path + "/" + x) for x in files_in_dir]
-        size_hs = convert_size(sum(size_list))
-        print("{}  {}".format(size_hs, os.getcwd()))
+        size_hs = convert_size(sum(size_list),os.getcwd())
         os.chdir(work_dir)
+        print(size_hs)
     except FileNotFoundError:
         print("File or dir not found !")
 
